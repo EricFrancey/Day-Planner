6976ad93224c007ccdbb5a48ebb0d1266288fd2a
Eric Francey <eric.g.francey@gmail.com>
2022-01-28 23:00:19 -0500

    First commit

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// Display current day
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+// IN PROGRESS:
+
+// Changing row colours based off of time of day
+// interactive
+// local storage of items
+
